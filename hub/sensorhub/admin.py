8a84875cd5ad3238b85581dd34e580b9b3c1acf0
Konrad Blum <konrad.blum@gmail.com>
2015-07-26 11:23:59 +0200

    Add "ID" column to model admin list views.

--- hub/sensorhub/admin.py
+++ hub/sensorhub/admin.py
@@ -3,27 +3,27 @@ from .models import Location, Deployment, Sensor, Reading, Token
 
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'enabled', 'created', 'modified',)
+    list_display = ('id', 'name', 'enabled', 'created', 'modified',)
     list_filter = ('enabled',)
 
 
 class DeploymentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location', 'enabled', 'created', 'modified',)
+    list_display = ('id', 'name', 'location', 'enabled', 'created', 'modified',)
     list_filter = ('enabled', 'location')
 
 
 class SensorAdmin(admin.ModelAdmin):
-    list_display = ('serial_number', 'deployment', 'enabled', 'created', 'modified',)
+    list_display = ('id', 'serial_number', 'deployment', 'enabled', 'created', 'modified',)
     list_filter = ('enabled', 'deployment',)
 
 
 class ReadingAdmin(admin.ModelAdmin):
-    list_display = ('sensor', 'deployment', 'temperature', 'created',)
+    list_display = ('id', 'sensor', 'deployment', 'temperature', 'created', 'modified',)
     list_filter = ('sensor', 'deployment',)
 
 
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ('name', 'api_key', 'api_secret', 'enabled', 'created', 'modified',)
+    list_display = ('id', 'name', 'api_key', 'api_secret', 'enabled', 'created', 'modified',)
     list_filter = ('enabled',)
 
 
